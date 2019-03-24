591cf71f49a1823c84cbe439901ce5165b0c4695
Unknown <sluigimuller@gmail.com>
2019-03-24 16:23:44 -0400

    Commit exercício aula 1 e 2 e copia de arquivo

---
+++ Copia de Arquivo/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *arquivoA = fopen("arquivoA.txt", "r");
+    FILE *arquivoB = fopen("ArquivoB.txt", "w");
+    printf("a");
+    char aux[30];
+    if (arquivoA == NULL) {
+        printf("aNao foi possivel abrir o arquivo\n");
+    } else {
+        printf("b");
+        while (fgets(aux, 29,arquivoA) != NULL){
+            printf("c");
+            fprintf(arquivoB, "%s", aux);
+        }
+    }
+    fclose(arquivoA);
+    fclose(arquivoB);
+    return 0;
+}
