6972222a52bcaa37d01cef7336a66073c11f6225
luigim1998 <sluigimuller@gmail.com>
2019-06-11 19:11:54 -0400

    Criado a forma de apagar na questao 4 da lista 2

--- Aula_11_06_kernel_module_lista/kernelzinho.c
+++ Aula_11_06_kernel_module_lista/kernelzinho.c
@@ -9,7 +9,7 @@
 #include <linux/fs.h>             // Header for the Linux file system support
 #include <linux/uaccess.h>        // Required for the copy to user function
 
-struct k_list {
+struct todo_list {
 	struct list_head test_list;
 	char temp[256];
 };
@@ -18,31 +18,31 @@ struct list_head test_head;
 
 static int __init list_init(void) {
 
-	struct k_list *one,*two,*three,*four,*five,*entry;
+	struct todo_list *one,*two,*three,*four,*five,*entry;
 	struct list_head *ptr;
 
-	one=kmalloc(sizeof(struct k_list *),GFP_KERNEL);
-	two=kmalloc(sizeof(struct k_list *),GFP_KERNEL);
-	three=kmalloc(sizeof(struct k_list *),GFP_KERNEL);
-	four=kmalloc(sizeof(struct k_list *),GFP_KERNEL);
-	five=kmalloc(sizeof(struct k_list *),GFP_KERNEL);
+	one=kmalloc(sizeof(struct todo_list *),GFP_KERNEL);
+	two=kmalloc(sizeof(struct todo_list *),GFP_KERNEL);
+	three=kmalloc(sizeof(struct todo_list *),GFP_KERNEL);
+	four=kmalloc(sizeof(struct todo_list *),GFP_KERNEL);
+	five=kmalloc(sizeof(struct todo_list *),GFP_KERNEL);
 
 	strcpy(one->temp,   "Abrir porta");
-	strcpy(two->temp,   "Beber chá");
+	strcpy(two->temp,   "Beber cha");
 	strcpy(three->temp, "Cozinhar miojo");
 	strcpy(four->temp,  "Deitar");
 	strcpy(five->temp,  "E rolar");
 
 	INIT_LIST_HEAD(&test_head);
 
-	list_add(&one->test_list,&test_head);
-	list_add(&two->test_list,&test_head);
-	list_add(&three->test_list,&test_head);
-	list_add(&four->test_list,&test_head);
-	list_add(&five->test_list,&test_head);
+	list_add_tail(&one->test_list,  &test_head);
+	list_add_tail(&two->test_list,  &test_head);
+	list_add_tail(&three->test_list,&test_head);
+	list_add_tail(&four->test_list, &test_head);
+	list_add_tail(&five->test_list, &test_head);
 
 	list_for_each(ptr,&test_head){
-		entry=list_entry(ptr,struct k_list,test_list);
+		entry=list_entry(ptr,struct todo_list,test_list);
 
 		printk(KERN_INFO "Tarefa: %s\n", entry->temp);
 
@@ -51,7 +51,15 @@ static int __init list_init(void) {
 
 }
 
-static void __exit list_exit(void) {
+static void __exit list_exit(void){
+	struct list_head *pos, *q;
+	struct todo_list * tmp;
+	list_for_each_safe(pos, q, &test_head){
+        tmp = list_entry(pos, struct todo_list, test_list);
+        printk(KERN_INFO "Apagando recado: %s\n", tmp->temp);
+        list_del(pos);
+    }
+
 	printk(KERN_INFO "Saindo do bagulho\n");
 }
 
